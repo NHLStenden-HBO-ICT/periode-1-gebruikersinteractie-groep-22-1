bd3fadd114201e60b01c53f7c1e19f7c3e44bed4
Danny <danny.kluitenberg@student.nhlstenden.com>
2024-10-08 15:23:39 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Slime_Busters;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Slime_Busters_Main_Menu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SpelenButtonClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new NamesPage();
+        }
+
+        private void WinkelButtonClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new WinkelPage();
+        }
+
+        private void StoppenButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ NamesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Slime_Busters_Main_Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Slime_Busters
+{
+    /// <summary>
+    /// Interaction logic for NamesPage.xaml
+    /// </summary>
+    public partial class NamesPage : Page
+    {
+        public NamesPage()
+        {
+            InitializeComponent();
+        }
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            Names.Content = new MainWindow();
+        }
+    }
+}
