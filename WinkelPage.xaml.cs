15a2261faa0ff89b7f0735953fd6442a4d75b209
Rutgerkootje <rutger.kootje.kootje@student.nhlstenden.com>
2024-10-14 14:13:37 +0200

    ..

--- WinkelPage.xaml.cs
+++ WinkelPage.xaml.cs
@@ -21,13 +21,39 @@ namespace Slime_Busters
     /// </summary>
     public partial class WinkelPage : Page
     {
+
+
+
+
         public WinkelPage()
         {
             InitializeComponent();
         }
+
+
+        private void AttackButton(object sender, RoutedEventArgs e)
+        {
+            // Voeg hier de logica toe die je wilt uitvoeren wanneer op de verdediging-knop wordt geklikt
+            Console.WriteLine("aanvalsknop aangeklikt");
+        }
+        
+        
+        private void DefenseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Voeg hier de logica toe die je wilt uitvoeren wanneer op de verdediging-knop wordt geklikt
+            Console.WriteLine("verdedigingsknop aangeklikt");
+        }
+        
+        
         private void BackButtonClick(object sender, RoutedEventArgs e)
         {
-            Winkel.Content = new MenuPage();
+  
+
+        }
+
+        private void Winkel_Navigated(object sender, NavigationEventArgs e)
+        {
+
         }
     }
 }
