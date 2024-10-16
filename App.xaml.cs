4188e8390cd126978d7bb8b89f8bc33afda4c0f2
Danny Kluitenberg <danny.kluitenberg@student.nhlstenden.com>
2024-10-16 16:40:40 +0200

    Update App.xaml.cs
    test

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,7 @@ using System.Windows;
 namespace Slime_Busters
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml test
     /// </summary>
     public partial class App : Application
     {
