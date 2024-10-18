dc996c7538678fa1fc840655f22522adf60f3cf4
torak190 <durk.t98@gmail.com>
2024-10-18 12:41:20 +0200

    Fixed collisions of bullets and slimes

--- PlayPage.xaml.cs
+++ PlayPage.xaml.cs
@@ -19,6 +19,8 @@ namespace Slime_Busters
 {
     public partial class PlayPage : Page
     {
+        #region Variables
+
         private bool moveLeftOne, moveRightOne;
         private bool moveLeftTwo, moveRightTwo;
         private double playerSpeed = 10;
@@ -26,9 +28,15 @@ namespace Slime_Busters
         private int slimeHealth = 10;
         private Dictionary<Rectangle, int> slimeHealthDictionary = new Dictionary<Rectangle, int>();
 
+        private List<Rectangle> bullets = new List<Rectangle>();
+        private List<Rectangle> slimes = new List<Rectangle>();
 
         private DispatcherTimer gameTimer;
         private DispatcherTimer spawnTimer;
+
+        #endregion
+
+        #region Constructor
 
         public PlayPage(string playerOneName, string playerTwoName)
         {
@@ -47,6 +55,10 @@ namespace Slime_Busters
             PlayerScreen.Focus();
         }
 
+        #endregion
+
+        #region Gameplay Loop
+
         private void GameTimer(object sender, EventArgs e)
         {
             if (moveLeftOne && Canvas.GetLeft(playerOne) > 0)
@@ -60,7 +72,13 @@ namespace Slime_Busters
                 Canvas.SetLeft(playerTwo, Canvas.GetLeft(playerTwo) + playerSpeed);
 
             ShootBullets();
+            MoveSlime();
+            CheckBulletSlimeCollision();
         }
+
+        #endregion
+
+        #region Player movement
 
         private void KeyIsDownOne(object sender, KeyEventArgs e)
         {
@@ -85,7 +103,6 @@ namespace Slime_Busters
             if (e.Key == Key.W)
                 MakeBullets(playerOne, 10, 780);
 
-
             if (e.Key == Key.J)
                 moveLeftTwo = false;
             if (e.Key == Key.L)
@@ -94,7 +111,12 @@ namespace Slime_Busters
             if (e.Key == Key.I)
                 MakeBullets(playerTwo, -10, 780);
         }
-        private void MakeBullets(Rectangle player, int direction, double playerCenterHeight)
+
+        #endregion
+
+        #region Bullets
+
+        private void MakeBullets(Rectangle player, int bulletDirection, double playerCenterHeight)
         {
             Rectangle bullet = new Rectangle
             {
@@ -104,40 +126,43 @@ namespace Slime_Busters
             };
 
             Canvas.SetTop(bullet, Canvas.GetBottom(player) + playerCenterHeight - (bullet.Height / 2));
-            Canvas.SetLeft(bullet, Canvas.GetLeft(player) + (direction > 0 ? player.Width : -bullet.Width));
+            Canvas.SetLeft(bullet, Canvas.GetLeft(player) + (bulletDirection > 0 ? player.Width : -bullet.Width));
 
             PlayerScreen.Children.Add(bullet);
-
-            bullet.Tag = direction;
+            bullet.Tag = bulletDirection;
+            bullets.Add(bullet);
         }
 
         private void ShootBullets()
         {
-            foreach (UIElement element in PlayerScreen.Children)
+            for (int i = bullets.Count - 1; i >= 0; i--)
             {
-                if (element is Rectangle bullet && bullet.Tag != null)
+                Rectangle bullet = bullets[i];
+                if (bullet.Tag != null)
                 {
-                    int direction = (int)bullet.Tag;
-
-                    Canvas.SetLeft(bullet, Canvas.GetLeft(bullet) + direction);
+                    int bulletDirection = (int)bullet.Tag;
+                    Canvas.SetLeft(bullet, Canvas.GetLeft(bullet) + bulletDirection);
 
                     if (Canvas.GetLeft(bullet) < 0 || Canvas.GetLeft(bullet) > PlayerScreen.ActualWidth)
                     {
                         PlayerScreen.Children.Remove(bullet);
-                        break;
+                        bullets.RemoveAt(i);
                     }
                 }
             }
         }
+
+        #endregion
+
+        #region Slimes
+
         private void SpawnSlime(object sender, EventArgs e)
         {
             SpawnMovingSlime(slimeSpeed);
-
             SpawnMovingSlime(-slimeSpeed);
         }
 
-
-        private void SpawnMovingSlime(int direction)
+        private void SpawnMovingSlime(int slimeDirection)
         {
             Rectangle slime = new Rectangle
             {
@@ -152,43 +177,67 @@ namespace Slime_Busters
             Canvas.SetLeft(slime, centerX);
             Canvas.SetTop(slime, centerY);
 
-
             PlayerScreen.Children.Add(slime);
-
-            slime.Tag = direction;
-
+            slime.Tag = slimeDirection;
             slimeHealthDictionary[slime] = slimeHealth;
+            slimes.Add(slime);
         }
 
         private void MoveSlime()
         {
-            foreach (UIElement element in PlayerScreen.Children)
+            for (int i = slimes.Count - 1; i >= 0; i--)
             {
-                if (element is Rectangle slime && slime.Tag != null)
+                Rectangle slime = slimes[i];
+                if (slime.Tag != null)
                 {
-                    int direction = (int)slime.Tag;
+                    int slimeDirection = (int)slime.Tag;
+                    Canvas.SetLeft(slime, Canvas.GetLeft(slime) + slimeDirection);
 
-                    Canvas.SetLeft(slime, Canvas.GetLeft(slime) + direction);
                     if (Canvas.GetLeft(slime) < 0 || Canvas.GetLeft(slime) > PlayerScreen.ActualWidth)
                     {
                         PlayerScreen.Children.Remove(slime);
+                        slimeHealthDictionary.Remove(slime);
+                        slimes.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Collision Detection
+
+        private void CheckBulletSlimeCollision()
+        {
+            for (int i = bullets.Count - 1; i >= 0; i--)
+            {
+                Rectangle bullet = bullets[i];
+                for (int j = slimes.Count - 1; j >= 0; j--)
+                {
+                    Rectangle slime = slimes[j];
+
+                    if (IsColliding(bullet, slime))
+                    {
+                        PlayerScreen.Children.Remove(bullet);
+                        PlayerScreen.Children.Remove(slime);
+
+                        bullets.RemoveAt(i);
+                        slimeHealthDictionary.Remove(slime);
+                        slimes.RemoveAt(j);
                         break;
                     }
                 }
             }
         }
-        private void DamageSlime(Rectangle slime, int damage)
-        {
-            if (slimeHealthDictionary.ContainsKey(slime))
-            {
-                slimeHealthDictionary[slime] -= damage;
 
-                if (slimeHealthDictionary[slime] <= 0)
-                {
-                    PlayerScreen.Children.Remove(slime);
-                    slimeHealthDictionary.Remove(slime);
-                }
-            }
+        private bool IsColliding(Rectangle bullet, Rectangle slime)
+        {
+            Rect bulletRect = new Rect(Canvas.GetLeft(bullet), Canvas.GetTop(bullet), bullet.Width, bullet.Height);
+            Rect slimeRect = new Rect(Canvas.GetLeft(slime), Canvas.GetTop(slime), slime.Width, slime.Height);
+
+            return bulletRect.IntersectsWith(slimeRect);
         }
+
+        #endregion
     }
 }
